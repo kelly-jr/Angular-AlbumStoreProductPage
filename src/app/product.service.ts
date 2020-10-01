9dd34befeb51f860746c51b3719ead78c6cf45dd
JR <kelly.mulumbi@strathmore.edu>
2020-10-01 18:42:58 +0300

    Adding interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 
 import 'rxjs/add/operator/map';
+import {Album} from './album';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -11,8 +13,8 @@ export class ProductService {
   constructor(private _http: Http) {
   }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response: Response) => response.json())
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response: Response) => <Album>response.json())
   }
 
 }
